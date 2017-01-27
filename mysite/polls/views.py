16aaa6d8f6f5a2aa4cb2b7665600edd4b778d77f
namelessking <habibur.tanjin@gmail.com>
2017-01-27 03:45:46 +0100

    added static files

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,7 @@
+from django.core.urlresolvers import reverse
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render,get_object_or_404
 from.models import Question
 # Create your views here.
 
@@ -10,11 +12,22 @@ def index(request):
     return render(request,'polls/index.html',context)
 
 def detail(request,question_id):
-    question = Question.objects.get(pk = question_id)
+    question = get_object_or_404(Question,pk = question_id)
     return render(request,'polls/detail.html',{'question': question})
 
 def results(request,question_id):
-    return HttpResponse('result of question: %s' % question_id)
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request,'polls/results.html',{'question':question})
 
 def vote(request,question_id):
-    return HttpResponse('vote on questions %s' % question_id)
+    question = get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk = request.POST['choice'])
+    except:
+        return render(request,'polls/detail.html',{'question':question,'error_massage':
+            'Please select a choice '})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('polls:results',args=(question_id,)))
